af7e8e1e10d151f61c7cc9cba137f8f7c56f8f82
Zachary Lund <zachary.lund@streamlabs.com>
2018-06-20 15:45:41 -0700

    compressor: Use gzip format compression

--- fl-compressor/compressor.js
+++ fl-compressor/compressor.js
@@ -25,23 +25,23 @@ async function ensure_path(filepath) {
     }
 }
 
-async function compressFiles(input_folder, output_folder, file_list, options) {
+async function compress_files(input_folder, output_folder, file_list, options) {
     const cwd = path.resolve();
 
     input_folder = path.resolve(cwd, input_folder);
     output_folder = path.resolve(cwd, output_folder);
 
     for (const file in file_list) {
-        const compressor = zlib.createDeflate();
+        const compressor = zlib.createGzip();
         const assumed_in_file = path.resolve(input_folder, file);
-        const out_file = path.resolve(output_folder, file + '.deflated');
+        const out_file = path.resolve(output_folder, file + '.gz');
 
         await ensure_path(out_file);
 
         const in_stream = fs.createReadStream(assumed_in_file);
         const out_stream = fs.createWriteStream(out_file);
 
-        const compressed = in_stream.pipe(compressor)
+        const compressed = in_stream.pipe(compressor);
 
         compressed.on('error', (error) => {
             console.log(`Failed to compress ${file}`);
