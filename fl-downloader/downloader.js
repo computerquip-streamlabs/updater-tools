e67e424026d6fef8192c27b80b726bec439ab0aa
Zachary Lund <zachary.lund@streamlabs.com>
2018-06-20 15:50:09 -0700

    downloader: Use gunzip and parallel promise queue
    This also changes export to expose actual export function

--- fl-downloader/downloader.js
+++ fl-downloader/downloader.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 const util = require('util');
 const axios = require('axios');
 const path = require('path');
+const slash = require('slash');
+const pmap = require('p-map');
 
 const mkdir = util.promisify(fs.mkdir);
-const inflate = util.promisify(zlib.inflate);
 
 async function mkdir_maybe(directory) {
     try {
@@ -28,52 +29,75 @@ async function ensure_path(filepath) {
 }
 
 async function handle_file(base_url, output_folder, file, options) {
-    const base_filename = path.basename(file, '.deflated');
-    const out_filepath = path.resolve(output_folder, path.dirname(file), base_filename);
-    const decompressor = zlib.createInflate();
+    const out_filepath = path.resolve(output_folder, file);
+    const decompressor = zlib.createGunzip();
     let response = null;
 
     try {
-        response =
-            await axios({
-                baseURL: base_url,
-                url: `${file}.deflated`,
-                responseType:'stream'
-            });
+        const url = `${base_url}/${slash(file)}.gz`;
+
+        const axios_options = {
+            url,
+            responseType:'stream'
+        };
+
+        response = await axios(axios_options);
 
         await ensure_path(out_filepath);
     } catch (error) {
-        console.log(`Failed to fetch file ${file} - ${error.response.status}`);
+        if (error)
+            console.log(`Failed to fetch file ${file} - ${error.response.status}`);
+
         return;
     }
 
     const out_stream = fs.createWriteStream(out_filepath);
     const decompressed = response.data.pipe(decompressor);
 
+    /* We only need to worry about the last writer
+     * in the chain to finish */
     decompressed.on('error', (error) => {
         console.log(`Failed to decompress ${file}`);
         console.log(error);
     });
 
-    decompressed.pipe(out_stream);
+    const out_pipe = decompressed.pipe(out_stream);
+
+    return new Promise((resolve, reject) => {
+        out_pipe.on('finish', resolve);
+        out_pipe.on('error', (error) => reject(error));
+    });
 }
 
-async function downloadFiles(base_url, output_folder, file_list, options) {
+async function download_files(base_url, output_folder, file_list, options) {
     const cwd = path.resolve();
-    let file_promises = [];
 
     output_folder = path.resolve(cwd, output_folder);
 
-    /* We should add a limit to the number of files we queue for download.
-     * We should be able to easily do this using Promise.race and checking
-     * promise state. */
-    for (const file in file_list) {
-        file_promises.push(handle_file(base_url, output_folder, file, options));
+    let download_list = [ ];
+    let progress_counter = 0; /* Starts at 0, ends at Object.keys(file_list).length */
+
+    /* Build a list of files we need to actually download */
+    const file_list_keys = Object.keys(file_list);
+
+    for (let idx = 0; idx < file_list_keys.length; ++idx) {
+        const file = file_list_keys[idx];
+
+        if (file_list[file] === null) continue;
+
+        download_list.push(file);
     }
 
-    await Promise.all(file_promises);
+    /* For each entry in the download_list... */
+    return pmap(download_list, (file) => {
+        ++progress_counter;
+
+        if (options.progress) {
+            options.progress(file, progress_counter / file_list_keys.length);
+        }
+
+        return handle_file(base_url, output_folder, file, options);
+    }, { concurrency: 10 });
 }
 
-module.exports = {
-    downloadFiles
-}
+module.exports = download_files;
