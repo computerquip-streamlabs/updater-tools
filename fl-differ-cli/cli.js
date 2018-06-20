583ab7628c044ef1a0a85ee27b616f7c4bb64fe7
Zachary Lund <zachary.lund@streamlabs.com>
2018-06-20 16:11:26 -0700

    differ: Update differ CLI to actually do something

--- fl-differ-cli/cli.js
+++ fl-differ-cli/cli.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
-const generator = require('fl-differ');
+const differ = require('fl-differ');
 const args = require('minimist')(process.argv.slice(2));
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
 
 function print_help() {
     const cwd = path.resolve();
     const node_name = process.argv[0];
     const script_name = path.relative(cwd, process.argv[1]);
-    console.log(`Usage: "${node_name}" "${script_name}" <from_file_list> <to_file_list>`);
+    console.log(`Usage: "${node_name}" "${script_name}" <from_file_list> <to_file_list> <diff_list_name>`);
 }
 
-if (args._.length < 2) {
+if (args._.length < 3) {
     print_help();
     return;
 }
@@ -28,6 +27,6 @@ const cwd = path.resolve();
 const from_list = JSON.parse(fs.readFileSync(path.resolve(cwd, args._[0])));
 const to_list = JSON.parse(fs.readFileSync(path.resolve(cwd, args._[1])));
 
-generator.generateDiffList(from_list, to_list, options, callback);
+differ(from_list, to_list, options, callback);
 
-console.log(result);
+fs.writeFileSync(path.resolve(cwd, args._[2]), JSON.stringify(result));
